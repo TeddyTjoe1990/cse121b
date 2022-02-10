4cd77db9bf97ee99826c379e3b8952b90cf9364b
TeddyTjoe1990 <her21059@byui.edu>
2022-02-10 17:46:30 +0700

    Some improvement are made

--- Week-06/js/script.js
+++ Week-06/js/script.js
@@ -1,20 +1,20 @@
 const api = {
     key: "93b04ac2e3dafc69e377e21afa088063",
-    base: "https://api.openweathermap.org/data/2.5/"
+    URL: "https://api.openweathermap.org/data/2.5/"
 }
 
 const searchbox = document.querySelector('.search-box');
 searchbox.addEventListener("keypress", setQuery);
 
-function setQuery(evt) {
-    if (evt.keyCode == 13) {
+function setQuery(event) {
+    if (event.keyCode == 13) {
         getResults(searchbox.value);
         console.log(searchbox.value);
     }
 }
 
 function getResults(query) {
-    fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+    fetch(`${api.URL}weather?q=${query}&units=metric&APPID=${api.key}`)
         .then(weather => {
             return weather.json();
         }).then(displayResults);
