7b01512ee102f12fc85747883c493f5016d7caf2
TeddyTjoe1990 <her21059@byui.edu>
2022-02-10 18:13:37 +0700

    update

--- Week-06/js/script.js
+++ Week-06/js/script.js
@@ -26,19 +26,19 @@ function displayResults(weather) {
 
     let now = new Date();
     let date = document.querySelector('.location .date');
-    date.innerText = dateBuilder(now);
+    date.innerText = fordate(now);
 
     let temp = document.querySelector('.current .temp');
     temp.innerHTML = `${Math.round(weather.main.temp)}<span>°c</span>`;
 
-    let weather_el = document.querySelector('.current .weather');
-    weather_el.innerText = weather.weather[0].main;
+    let wea_el = document.querySelector('.current .weather');
+    wea_el.innerText = weather.weather[0].main;
 
     let hilow = document.querySelector('.hi-low');
     hilow.innerText = `${Math.round(weather.main.temp_min)}°c / ${Math.round(weather.main.temp_max)}°c`;
 }
 
-function dateBuilder(d) {
+function fordate(d) {
     let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
     let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
